36dc1c0a1ce0bdcac67ee3c774f4334d3ebdbe20
josiahbryan <josiahbryan@6f7359d6-f641-0324-569b-6f61136d9b6b>
2012-01-18 22:21:09 +0000

    Completely cleaned up TrackingFilter - moved trackPoints to a member instead of a static function, cleaned up all the variable names, added a tuning method (tune())

--- trunk/devel/motionanal/TrackingFilter.cpp
+++ trunk/devel/motionanal/TrackingFilter.cpp
@@ -5,149 +5,27 @@
 
 #include "cv.h"
 #include "highgui.h"
+
 #include <stdio.h>
 #include <ctype.h>
 
 
-IplImage *image = 0, *grey = 0, *prev_grey = 0, *pyramid = 0, *prev_pyramid = 0, *swap_temp;
-
-int win_size = 10;
-const int MAX_COUNT = 500;
-CvPoint2D32f* points[2] = {0,0}, *swap_points;
-char* status = 0;
-int count = 0;
-int need_to_init = 0;
-int flags = 0;
-int originalCount = 0;
-
-int frameCounter = 0;
-
-QTime timeValue;
-bool timeInit = false;
-int timeFrameCounter = 0;
-
 //#define RESET_RATIO 0.75
- #define RESET_RATIO 0.75
-
-#define PointType_Static 0
-#define PointType_Live   1
-#define PointType_Wierd  2
-
-// 4 seconds of data at 30 frames a sec
-#define POINT_INFO_MAX_VALUES 120
-// Default to just a few frames over 30fps
-#define POINT_INFO_DEFAULT_NUM_VALUES 32
-
-class PointInfo 
-{
-public:
-	PointInfo() { reset(); }
-	
-	void reset() 
-	{
-		type = PointType_Static; // mover
-		deltaCounter = 0;
-		valid = true;
-		for(int i=0; i<POINT_INFO_MAX_VALUES; i++)
-			values[i] = 0;
-		valuesIdx = 0;
-		valuesTotal = 0;
-		numValuesUsed = POINT_INFO_DEFAULT_NUM_VALUES;
-		valuesAvg = 0;
-		lastValuesAvg = 0;
-		distanceMovedChangeCount = 0;
-	};
-	
-	inline float storeValue(float value)
-	{
-		// Store last average for other uses
-		lastValuesAvg = valuesAvg;
-		
-		valuesTotal -= values[valuesIdx];
-		valuesTotal += value;
-		values[valuesIdx] = value;
-		
-		valuesIdx ++;
-		int max = MIN(numValuesUsed, POINT_INFO_MAX_VALUES);
-		if(valuesIdx >= max)
-			valuesIdx = 0;
-		
-		valuesAvg = valuesTotal / (float)max;
-		return valuesAvg;
-	}
-	
-	inline float storeChangeValue(float value)
-	{
-		// Store last average for other uses
-		//lastValuesAvg = valuesAvg;
-		
-		changeValuesTotal -= changeValues[changeValuesIdx];
-		changeValuesTotal += value;
-		changeValues[changeValuesIdx] = value;
-		
-		changeValuesIdx ++;
-		int max = 3;//MIN(numValuesUsed, POINT_INFO_MAX_VALUES);
-		if(changeValuesIdx >= max)
-			changeValuesIdx = 0;
-		
-		changeValuesAvg = changeValuesTotal / (float)max;
-		return changeValuesAvg;
-	}
-	
-	float type;
-	float deltaCounter;
-	bool valid;
-	float values[POINT_INFO_MAX_VALUES];
-	float valuesTotal;
-	float valuesAvg;
-	float lastValuesAvg;
-	int valuesIdx;
-	int numValuesUsed;
-	int distanceMovedChangeCount;
-	double distanceChangeFrequency;
-	
-	float changeValues[POINT_INFO_MAX_VALUES];
-	float changeValuesTotal;
-	float changeValuesAvg;
-	int changeValuesIdx; 
-};
-
-PointInfo pointInfo[MAX_COUNT];
+#define RESET_RATIO 0.25
 
 
-QList<int> m_history;
-	
-/// Round up to next higher power of 2 (return x if it's already a power
-/// of 2).
-/// From: http://stackoverflow.com/questions/364985/algorithm-for-finding-the-smallest-power-of-two-thats-greater-or-equal-to-a-giv
-inline int pow2roundup (int x)
-{
-    if (x < 0)
-        return 0;
-    --x;
-    x |= x >> 1;
-    x |= x >> 2;
-    x |= x >> 4;
-    x |= x >> 8;
-    x |= x >> 16;
-    return x+1;
-}
-
-
-
-
-QImage trackPoints(QImage img)
+QImage TrackingFilter::trackPoints(QImage img)
 {
 	IplImage* frame = 0;
-	int i, k, c;
+	int i, k;
 	
-	if(!timeInit)
+	if(!m_timeInit)
 	{
-		timeInit = true;
-		timeValue.start();
-		timeFrameCounter = 0;
+		m_timeInit = true;
+		m_timeValue.start();
+		m_timeFrameCounter = 0;
 	}
-	timeFrameCounter++;
+	m_timeFrameCounter++;
 
 	if(img.format() != QImage::Format_RGB888)
 		img = img.convertToFormat(QImage::Format_RGB888);
@@ -158,39 +36,43 @@ QImage trackPoints(QImage img)
 	frame = cvCreateImageHeader( cvSize(img.width(), img.height()), IPL_DEPTH_8U, 3);
 	frame->imageData = (char*) img.bits();
 
-	if( !image )
+	if( !m_iplImage )
 	{
 		/* allocate all the buffers */
-		image = cvCreateImage( cvGetSize(frame), 8, 3 );
-		image->origin = frame->origin;
-		grey = cvCreateImage( cvGetSize(frame), 8, 1 );
-		prev_grey = cvCreateImage( cvGetSize(frame), 8, 1 );
-		pyramid = cvCreateImage( cvGetSize(frame), 8, 1 );
-		prev_pyramid = cvCreateImage( cvGetSize(frame), 8, 1 );
-		points[0] = (CvPoint2D32f*)cvAlloc(MAX_COUNT*sizeof(points[0][0]));
-		points[1] = (CvPoint2D32f*)cvAlloc(MAX_COUNT*sizeof(points[0][0]));
-		status = (char*)cvAlloc(MAX_COUNT);
-		flags = 0;
+		m_iplImage = cvCreateImage( cvGetSize(frame), 8, 3 );
+		m_iplImage->origin = frame->origin;
+		
+		m_greyImg	= cvCreateImage( cvGetSize(frame), 8, 1 );
+		m_prevGreyImg	= cvCreateImage( cvGetSize(frame), 8, 1 );
+		m_pyramid	= cvCreateImage( cvGetSize(frame), 8, 1 );
+		m_prevPyramid	= cvCreateImage( cvGetSize(frame), 8, 1 );
+		
+		m_currPoints = (CvPoint2D32f*)cvAlloc( MAX_COUNT * sizeof(CvPoint2D32f));
+		m_prevPoints = (CvPoint2D32f*)cvAlloc( MAX_COUNT * sizeof(CvPoint2D32f));
+		m_cvStatus = (char*)cvAlloc(MAX_COUNT);
+		m_cvFlags = 0;
 	}
 
-	cvCopy( frame, image, 0 );
-	cvCvtColor( image, grey, CV_BGR2GRAY );
+	cvCopy( frame, m_iplImage, 0 );
+	cvCvtColor( m_iplImage, m_greyImg, CV_BGR2GRAY );
 	
- 	QList<QPoint> pointsList;
+ 	//QList<QPoint> pointsList;
 	
-	if( need_to_init )
+	
+	int win_size = 10;
+	if( m_needInit )
 	{
 		/* automatic initialization */
-		IplImage* eig  = cvCreateImage( cvGetSize(grey), 32, 1 );
-		IplImage* temp = cvCreateImage( cvGetSize(grey), 32, 1 );
+		IplImage* eig  = cvCreateImage( cvGetSize(m_greyImg), 32, 1 );
+		IplImage* temp = cvCreateImage( cvGetSize(m_greyImg), 32, 1 );
 		double quality = 0.01;
 		double min_distance = 10;
-	
-		count = MAX_COUNT;
-		cvGoodFeaturesToTrack( grey, eig, temp, points[1], &count,
+		
+		m_validCount = MAX_COUNT;
+		cvGoodFeaturesToTrack( m_greyImg, eig, temp, m_currPoints, &m_validCount,
 					quality, min_distance, 0, 3, 0, 0.04 );
 					
-		cvFindCornerSubPix( grey, points[1], count,
+		cvFindCornerSubPix( m_greyImg, m_currPoints, m_validCount,
 			cvSize(win_size,win_size), cvSize(-1,-1),
 			cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03));
 			
@@ -198,121 +80,97 @@ QImage trackPoints(QImage img)
 		cvReleaseImage( &temp );
 		
 		for(i=0; i<MAX_COUNT; i++)
-		{
-			pointInfo[i].reset();
-		}
+			m_pointInfo[i].reset();
 		
-		originalCount = count;
-		frameCounter = 0;
-
+		m_foundCount = m_validCount;
 	}
-	else if( count > 0 )
+	else if( m_validCount > 0 )
 	{
-		frameCounter ++;
-		
-		cvCalcOpticalFlowPyrLK( prev_grey, grey, prev_pyramid, pyramid, 
-			points[0], points[1], count, cvSize(win_size,win_size), 3, status, 0,
-			cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03), flags );
+		cvCalcOpticalFlowPyrLK( m_prevGreyImg, m_greyImg, m_prevPyramid, m_pyramid, 
+			m_prevPoints, m_currPoints, m_validCount, cvSize(win_size,win_size), 3, m_cvStatus, 0,
+			cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03), m_cvFlags );
 			
-		flags |= CV_LKFLOW_PYR_A_READY;
+		m_cvFlags |= CV_LKFLOW_PYR_A_READY;
 		
-		for( i = k = 0; i < count; i++ )
+		for( i = k = 0; i < m_validCount; i++ )
 		{
-			if( !status[i] )
+			if( !m_cvStatus[i] )
 			{
-				pointInfo[i].valid = false;
+				m_pointInfo[i].valid = false;
 				continue;
 			}
 		
 			k++;
 			
-			pointsList << QPoint(points[1][i].x, points[1][i].y);
+			//pointsList << QPoint(points[1][i].x, points[1][i].y);
 		}
-		count = k;
+		m_validCount = k;
 	}
 
-	
-	
-	//QList<QPoint> points = trackPoints(img);
 	
 	QImage imageCopy = img.copy();
 	QPainter p(&imageCopy);
 		
-	//int min = 25;
-	int bucketCutoff = 25; /// Magic number
-	
-	//double rateCutoff = 1.0; /// Magic number
-	//int fps = 30; /// Just a guess
-	
-	int elapsed = timeValue.elapsed();
-	double fps = ((double)elapsed) / ((double)timeFrameCounter) / 2.0;
-	//qDebug() << "fps:"<<fps<<", elapsed:"<<elapsed<<", timeFrameCounter:"<<timeFrameCounter; 
-	
-	int distatnceChangeOkCount = 0;
+	int elapsed = m_timeValue.elapsed();
+	double fps = ((double)elapsed) / ((double)m_timeFrameCounter) / 2.0;
 	
 	// Calculate movement vectors (distances between last positions)
-	for (i=0; i<originalCount; i++) 
+	for (i=0; i<m_foundCount; i++) 
 	{
 		float dX, dY;
 		float distSquared;
-		if (status[i] && pointInfo[i].valid)	// Make sure its an existing vector 
+		if (m_pointInfo[i].valid)	// Make sure its an existing vector 
 		{	
-			dX = points[1][i].x - points[0][i].x;
-			dY = points[1][i].y - points[0][i].y;
+			dX = m_currPoints[i].x - m_prevPoints[i].x;
+			dY = m_currPoints[i].y - m_prevPoints[i].y;
 			
 			// Make sure its not massive
 			distSquared = dX*dX + dY*dY;
 			
-			float avg = pointInfo[i].storeValue(abs(distSquared));
+			float avg = m_pointInfo[i].storeValue(fabs(distSquared));
 			
-			float distDiff = abs(pointInfo[i].lastValuesAvg - avg);
+			float distDiff = fabs(m_pointInfo[i].lastValuesAvg - avg);
 			bool hasChanged = distDiff > 0.001;
-			pointInfo[i].distanceMovedChangeCount += hasChanged ? 1 : -1;
-			if(pointInfo[i].distanceMovedChangeCount > fps)
-				pointInfo[i].distanceMovedChangeCount --;
-			if(pointInfo[i].distanceMovedChangeCount < 0)
-				pointInfo[i].distanceMovedChangeCount = 0;
-			pointInfo[i].distanceChangeFrequency = pointInfo[i].distanceMovedChangeCount / fps;
+			m_pointInfo[i].distanceMovedChangeCount += hasChanged ? 1 : -1;
+			if(m_pointInfo[i].distanceMovedChangeCount > fps)
+				m_pointInfo[i].distanceMovedChangeCount --;
+			if(m_pointInfo[i].distanceMovedChangeCount < 0)
+				m_pointInfo[i].distanceMovedChangeCount = 0;
+			m_pointInfo[i].distanceChangeFrequency = m_pointInfo[i].distanceMovedChangeCount / fps;
 			
-			pointInfo[i].storeChangeValue(pointInfo[i].distanceMovedChangeCount);
+			m_pointInfo[i].storeChangeValue(m_pointInfo[i].distanceMovedChangeCount);
 		}
 	}
 	
 	
 	QFont font = p.font();
-	p.setFont(QFont("", 8));
-	
+	if(!m_cleanOutput)
+		p.setFont(QFont("", 8));
+		
 	float moveSum = 0;
 	int moveCount = 0;
-	for (i=0; i<originalCount; i++) 
+	for (i=0; i<m_foundCount; i++) 
 	{
-		if (status[i] && pointInfo[i].valid)	// Make sure its an existing vector 
+		if (m_pointInfo[i].valid)	// Make sure its an existing vector 
 		{	
-			QPoint point(points[1][i].x, points[1][i].y);
+			QPoint point((int)m_currPoints[i].x, (int)m_currPoints[i].y);
 			
 			//if (decays[i] == 1.0 || (counter > 0.1 && /*bucket >= 0 && */bucket < bucketCutoff))
-			if(pointInfo[i].valuesAvg > 0.075 //)
-			&&
-			   pointInfo[i].changeValuesAvg > 0.1) 
+			if(m_pointInfo[i].valuesAvg > m_userTuningMinMove &&
+			(m_userTuningUseChangeValues ? m_pointInfo[i].changeValuesAvg > m_userTuningMinChange : true)) 
 			{
-				// app-spec code
-				//if(/*decays[i] == 1.0 || */distanceChangeCount[i])
+				if(!m_cleanOutput)
 				{
+			
 					p.setBrush(Qt::green);
 					p.drawEllipse(QRect(point - QPoint(5,5), QSize(10,10)));
 					//p.drawText(point + QPoint(-5,5), QString("%1").arg(i));
-					
-// 					moveSum += counter;
-// 					moveCount ++;
 				}
 				
-				moveSum += pointInfo[i].valuesAvg;
+				moveSum += m_pointInfo[i].valuesAvg;
 				moveCount ++;
 
 			}
-			
-			//qDebug() << i << moveSum; //pointInfo[i].valuesAvg;
-			
 // 			else
 // 			{
 // 				p.setBrush(Qt::gray);
@@ -321,106 +179,130 @@ QImage trackPoints(QImage img)
 
 		}
 	}
- 	p.setFont(font);
- 	
- 	
- 	
- 	
- 	
+	if(!m_cleanOutput)
+		p.setFont(font);
 	
-	int moveAvg = moveSum / (!moveCount?1:moveCount);
+	
+	int moveAvg = (int)(moveSum / (!moveCount?1:moveCount));
 	
 	//qDebug() << "moveAvg:"<<moveAvg;
 	m_history << moveAvg;
 	if(m_history.size() > imageCopy.size().width())
 		m_history.takeFirst();
 	
-	int historyMax = 0;
-	foreach(int val, m_history)
-		if(val > historyMax)
-			historyMax = val;
 	
-	if(historyMax<=0)
-		historyMax = 1;
-		
-	//if(historyMax > 10000)
-	//historyMax = 50000;
-	//historyMax = 50;
+	m_historyWindowTotal -= m_historyWindow[m_historyWindowIndex];
+	m_historyWindowTotal += moveAvg;
+	m_historyWindow[m_historyWindowIndex] = moveAvg;
+	m_historyWindowIndex ++;
+	if(m_historyWindowIndex >= m_historyWindowSize)
+		m_historyWindowIndex = 0;
+		 
+	m_historyWindowAverage = m_historyWindowTotal / m_historyWindowSize;
+	if(m_historyWindowAverage < 0)
+		m_historyWindowAverage = 1;
 	
-	historyMax /= 2;
-	
-	//qDebug() << "historyMax:"<<historyMax<<", moveAvg:"<<moveAvg;
-	
-	int lineMaxHeight = imageCopy.size().height() * .25;
-	int margin = 0;
-	int lineZeroY = imageCopy.size().height() - margin;
-	int lineX = margin;
-	int lineLastY = lineZeroY;
-	
-	int lastLineHeight = 0;
-	
-	int textInc = 10;
-	
-	p.setPen(Qt::red);
-	for(i=0; i<m_history.size(); i++)
+	if(!m_cleanOutput)
 	{
+		int historyMax = 0;
+		foreach(int val, m_history)
+			if(val > historyMax)
+				historyMax = val;
+		
+		if(historyMax<=0)
+			historyMax = 1;
+			
+		//if(historyMax > 10000)
+		//historyMax = 50000;
+		//historyMax = 200;
+		
+		historyMax /= 2;
+		
+		//qDebug() << "historyMax:"<<historyMax<<", moveAvg:"<<moveAvg;
+		
+		int lineMaxHeight = (int)((double)imageCopy.size().height() * .25);
+		int margin = 0;
+		int lineZeroY = imageCopy.size().height() - margin;
+		int lineLastY = lineZeroY;
+		
+		int lastLineHeight = 0;
+		
+		int textInc = 25;
+		
 		p.setPen(Qt::red);
-		
-		int value = m_history[i];
-		double ratio = ((double)value/(double)historyMax);
-		int lineHeight = (int)(
-					ratio *  
-					(lineMaxHeight - (margin*2))
-				);
-			
-		int lineTop = lineZeroY - lineHeight;
-		
-		//p.setPen(QColor(255,0,0, (int)((double)i / (double)(imageCopy.size().width()) * 255.f)));
-		p.setPen(Qt::white);
-			
-		p.drawLine(i+1,lineLastY+1,i+2,lineTop+1);
-		
-		p.setPen(Qt::red);
-			
-		p.drawLine(i,lineLastY,i+1,lineTop);
-		
-		if(i % textInc == 0)
+		for(i=0; i<m_history.size(); i++)
 		{
-			p.setPen(QColor(0,0,0));//, (int)((double)(i/textInc) / (double)(imageCopy.size().width()/textInc) * 255.f)));
-			p.drawText( QPoint( i+2, lineTop+1 ), QString("%1").arg((int)(ratio*100)) );
-			p.setPen(QColor(255,255,255));//, (int)((double)(i/textInc) / (double)(imageCopy.size().width()/textInc) * 255.f)));
-			p.drawText( QPoint( i+1, lineTop ), QString("%1").arg((int)(ratio*100)) );
+			p.setPen(Qt::red);
+			
+			int value = m_history[i];
+			double ratio = ((double)value/(double)historyMax);
+			int lineHeight = (int)(
+						ratio *  
+						(lineMaxHeight - (margin*2))
+					);
+				
+			int lineTop = lineZeroY - lineHeight;
+			
+			//p.setPen(QColor(255,0,0, (int)((double)i / (double)(imageCopy.size().width()) * 255.f)));
+			p.setPen(Qt::white);
+				
+			p.drawLine(i+1,lineLastY+1,i+2,lineTop+1);
+			
+			p.setPen(Qt::red);
+				
+			p.drawLine(i,lineLastY,i+1,lineTop);
+			
+			if(i % textInc == 0)
+			{
+				//int val = ratio*100;
+				int val = value;
+				p.setPen(QColor(0,0,0));//, (int)((double)(i/textInc) / (double)(imageCopy.size().width()/textInc) * 255.f)));
+				p.drawText( QPoint( i+2, lineTop+1 ), QString("%1").arg((int)(val)) );
+				p.setPen(QColor(255,255,255));//, (int)((double)(i/textInc) / (double)(imageCopy.size().width()/textInc) * 255.f)));
+				p.drawText( QPoint( i+1, lineTop ), QString("%1").arg((int)(val)) );
+			}
+			
+			//qDebug() << "value:"<<value<<", lineHeight:"<<lineHeight<<", lineMaxHeight:"<<lineMaxHeight<<", margin:"<<margin<<", lineTop:"<<lineTop;
+			
+			lineLastY = lineTop;
+			//lastLineHeight = lineHeight;
+			lastLineHeight = (int)(ratio*100);
 		}
-		
-		//qDebug() << "value:"<<value<<", lineHeight:"<<lineHeight<<", lineMaxHeight:"<<lineMaxHeight<<", margin:"<<margin<<", lineTop:"<<lineTop;
-		
-		lineLastY = lineTop;
-		//lastLineHeight = lineHeight;
-		lastLineHeight = ratio*100;
+	
+		p.setFont(QFont("", 24, QFont::Bold));
+		p.setPen(Qt::black);
+		p.drawText(imageCopy.rect().topRight() + QPoint( -149, 31 ), QString().sprintf("%06d", m_historyWindowAverage)); //moveAvg));
+		p.setPen(Qt::white);
+		p.drawText(imageCopy.rect().topRight() + QPoint( -150, 30 ), QString().sprintf("%06d", m_historyWindowAverage)); //moveAvg));
+		p.setFont(font);
+	} 
+	
+	if(m_historyWindowAverage <= 0)
+	{
+		//if(!m_cleanOutput)
+			//qDebug() << QTime::currentTime().toString() << " - HistoryAverage reached ZERO";
+		emit historyAvgZero();
 	}
-
-
-
-
-
+	
+	emit historyAvg(m_historyWindowAverage);
+	emit averageMovement(moveAvg);
 	
 	
+	CV_SWAP( m_prevGreyImg, m_greyImg, m_swapTemp );
+	CV_SWAP( m_prevPyramid, m_pyramid, m_swapTemp );
+	CV_SWAP( m_prevPoints, m_currPoints, m_swapPoints );
+	m_needInit = 0;
 	
-	CV_SWAP( prev_grey, grey, swap_temp );
-	CV_SWAP( prev_pyramid, pyramid, swap_temp );
-	CV_SWAP( points[0], points[1], swap_points );
-	need_to_init = 0;
-	//cvShowImage( "LkDemo", image );
-		
-	if(count <= originalCount * RESET_RATIO)
+	if(m_validCount <= m_foundCount * m_resetRatio)
 	{
 		//qDebug() << "Need to init!";
-		qDebug() << "count less than "<<(originalCount * RESET_RATIO)<<", resetting.";
-		need_to_init = 1;
+		//if(!m_cleanOutput)
+		//	qDebug() << "count less than "<<(m_foundCount * RESET_RATIO)<<", resetting.";
+		m_needInit = 1;
 	}
 	else
 	{
-		//qDebug() << "Count:"<<count<<", reset point:"<<(originalCount * RESET_RATIO);
+		//qDebug() << "Count:"<<count<<", reset point:"<<(m_foundCount * RESET_RATIO);
 	}
 	
 	cvReleaseImage(&frame);
@@ -433,7 +315,41 @@ QImage trackPoints(QImage img)
 
 TrackingFilter::TrackingFilter(QObject *parent)
 	: VideoFilter(parent)
+	, m_cleanOutput(false)
 {
+	
+	m_iplImage = 0;
+	m_greyImg = 0;
+	m_prevGreyImg = 0;
+	m_pyramid = 0;
+	m_prevPyramid = 0;
+	m_swapTemp = 0;
+	
+	m_currPoints = 0;
+	m_prevPoints = 0;
+	m_swapPoints = 0;
+	m_cvStatus = 0;
+	m_validCount = 0;
+	m_needInit = 0;
+	m_cvFlags = 0;
+	m_foundCount = 0;
+	
+	m_timeInit = false;
+	m_timeFrameCounter = 0;
+	
+	m_userTuningMinMove   = TRACKING_DEFAULT_MIN_MOVE; // default 0.075
+	m_userTuningMinChange = TRACKING_DEFAULT_MIN_CHANGE; // default 0.1
+	m_userTuningUseChangeValues = true;
+	
+	for(int i=0; i<HISTORY_MAX_WINDOW_SIZE; i++)
+		m_historyWindow[i] = 0;
+	
+	m_historyWindowTotal = 0;
+	m_historyWindowSize = HISTORY_MAX_WINDOW_SIZE / 10;
+	m_historyWindowAverage = 0;
+	m_historyWindowIndex = 0;
+	
+	m_resetRatio = RESET_RATIO;
 }
 
 TrackingFilter::~TrackingFilter()
@@ -443,20 +359,29 @@ TrackingFilter::~TrackingFilter()
 void TrackingFilter::processFrame()
 {
 	QImage image = frameImage();	
-	QImage histo = highlightFaces(image);
+	QImage histo = trackPoints(image);
 	
 	enqueue(new VideoFrame(histo,m_frame->holdTime()));
 }
 
-QImage TrackingFilter::highlightFaces(QImage img)
+void TrackingFilter::tune(float minMove, bool useChange, int minChange, int numValues, int numChangeValues, int historyWindowSize, float resetRatio)
 {
-#if 0
-	return img.copy();
-#else
-	return trackPoints(img);
-#endif
+	for(int i=0; i<MAX_COUNT; i++)
+	{
+		if(numValues > 0)
+			m_pointInfo[i].userTuningNumValues = numValues;
+		if(numChangeValues > 0)
+			m_pointInfo[i].userTuningNumChangeValues = numChangeValues;
+	}
+	
+	m_userTuningMinMove   = minMove   >= 0 ? minMove   : TRACKING_DEFAULT_MIN_MOVE;
+	m_userTuningMinChange = minChange >= 0 ? minChange : TRACKING_DEFAULT_MIN_CHANGE;
+	m_userTuningUseChangeValues = useChange;
+	
+	m_historyWindowSize = historyWindowSize > 0 ? 
+		(historyWindowSize >= HISTORY_MAX_WINDOW_SIZE ? HISTORY_MAX_WINDOW_SIZE : historyWindowSize)
+		:
+		HISTORY_MAX_WINDOW_SIZE / 10;
+		
+	m_resetRatio = resetRatio > 0.f && resetRatio < 1.f ? resetRatio : RESET_RATIO;
 }
-
-
-
-
--- trunk/devel/motionanal/TrackingFilter.h
+++ trunk/devel/motionanal/TrackingFilter.h
@@ -4,7 +4,117 @@
 #include <QtGui>
 #include "VideoFilter.h"
 
-class EyeCounter;
+
+#define MAX_COUNT 500
+
+/*
+#define PointType_Static 0
+#define PointType_Live   1
+#define PointType_Wierd  2*/
+
+// 4 seconds of data at 30 frames a sec
+#define POINT_INFO_MAX_VALUES 120
+// Default to just a few frames over 30fps
+#define POINT_INFO_DEFAULT_NUM_VALUES 32
+#define POINT_INFO_DEFAULT_NUM_CHANGE_VALUES 5
+
+#define TRACKING_DEFAULT_MIN_MOVE 0.075
+#define TRACKING_DEFAULT_MIN_CHANGE 0.1
+
+#include "cv.h"
+#include "highgui.h"
+
+class PointInfo 
+{
+public:
+	PointInfo() 
+	{
+		userTuningNumValues = 0;
+		userTuningNumChangeValues = 0;
+		reset(); 
+	}
+	
+	void reset() 
+	{
+		//type = PointType_Static; // mover
+		deltaCounter = 0;
+		valid = true;
+		for(int i=0; i<POINT_INFO_MAX_VALUES; i++)
+		{
+			values[i] = 0;
+			changeValues[i] = 0;
+		}
+		valuesIdx = 0;
+		valuesTotal = 0;
+		numValuesUsed       = userTuningNumValues > 0 ? userTuningNumValues : POINT_INFO_DEFAULT_NUM_VALUES;
+		numChangeValuesUsed = userTuningNumChangeValues > 0 ? userTuningNumChangeValues : POINT_INFO_DEFAULT_NUM_CHANGE_VALUES;
+		valuesAvg = 0;
+		lastValuesAvg = 0;
+		distanceMovedChangeCount = 0;
+		changeValuesTotal = 0;
+		changeValuesAvg = 0;
+		changeValuesIdx = 0;
+	};
+	
+	inline float storeValue(float value)
+	{
+		// Store last average for other uses
+		lastValuesAvg = valuesAvg;
+		
+		valuesTotal -= values[valuesIdx];
+		valuesTotal += value;
+		values[valuesIdx] = value;
+		
+		valuesIdx ++;
+		int max = numValuesUsed < POINT_INFO_MAX_VALUES ? numValuesUsed : POINT_INFO_MAX_VALUES;
+		if(valuesIdx >= max)
+			valuesIdx = 0;
+		
+		valuesAvg = valuesTotal / (float)max;
+		return valuesAvg;
+	}
+	
+	inline float storeChangeValue(float value)
+	{
+		changeValuesTotal -= changeValues[changeValuesIdx];
+		changeValuesTotal += value;
+		changeValues[changeValuesIdx] = value;
+		
+		changeValuesIdx ++;
+		
+		int max = numChangeValuesUsed < POINT_INFO_MAX_VALUES ? numChangeValuesUsed : POINT_INFO_MAX_VALUES;
+		if(changeValuesIdx >= max)
+			changeValuesIdx = 0;
+		
+		changeValuesAvg = changeValuesTotal / (float)max;
+		return changeValuesAvg;
+	}
+	
+	//float type;
+	float deltaCounter;
+	bool valid;
+	float values[POINT_INFO_MAX_VALUES];
+	float valuesTotal;
+	float valuesAvg;
+	float lastValuesAvg;
+	int valuesIdx;
+	int numValuesUsed;
+	
+	int distanceMovedChangeCount;
+	double distanceChangeFrequency;
+	
+	float changeValues[POINT_INFO_MAX_VALUES];
+	float changeValuesTotal;
+	float changeValuesAvg;
+	int changeValuesIdx; 
+	int numChangeValuesUsed;
+	
+	int userTuningNumValues;
+	int userTuningNumChangeValues;
+};
+
+
+#define HISTORY_MAX_WINDOW_SIZE 300
 
 class TrackingFilter : public VideoFilter
 {
@@ -13,17 +123,57 @@ public:
 	TrackingFilter(QObject *parent=0);
 	~TrackingFilter();
 	
-signals:
-	void facesFound(QList<QRect>);
+	void tune(float minMove=-1, bool useChange=true, int minChange=-1, int numValues=-1, int numChangeValues=-1, int historyWindowSize=-1, float resetRatio=-1);
 	
+	void setCleanOutput(bool flag){  m_cleanOutput = flag; }
+	
+signals:
+	void historyAvgZero();
+	void historyAvg(int);
+	void averageMovement(int);
+
 protected:
 	virtual void processFrame();
 	
-	QImage highlightFaces(QImage);
+	QImage trackPoints(QImage);
 	
-	EyeCounter *m_counter;
+private:
+	IplImage *m_iplImage,
+		*m_greyImg,
+		*m_prevGreyImg,
+		*m_pyramid,
+		*m_prevPyramid,
+		*m_swapTemp;
 	
+	CvPoint2D32f *m_currPoints, *m_prevPoints, *m_swapPoints;
+	char* m_cvStatus;
+	int m_validCount;
+	int m_needInit;
+	int m_cvFlags;
 	
+	int m_foundCount;
+	
+	QTime m_timeValue;
+	bool m_timeInit;
+	int m_timeFrameCounter;
+	
+	float m_userTuningMinMove; // default 0.075
+	float m_userTuningMinChange; // default 0.1;
+	bool m_userTuningUseChangeValues;
+	
+	PointInfo m_pointInfo[MAX_COUNT];
+	
+	QList<int> m_history;
+	
+	int m_historyWindow[HISTORY_MAX_WINDOW_SIZE];
+	int m_historyWindowTotal;
+	int m_historyWindowSize;
+	int m_historyWindowAverage;
+	int m_historyWindowIndex;
+	
+	float m_resetRatio;
+	
+	bool m_cleanOutput;
 
 
 };

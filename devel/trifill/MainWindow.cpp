742279981724f3bb7595f5b03db5017a8a426489
josiahbryan <josiahbryan@6f7359d6-f641-0324-569b-6f61136d9b6b>
2012-07-05 16:50:40 +0000

    Added a triangle fill sample, with color interpolation between each vertex

---
+++ devel/trifill/MainWindow.cpp
@@ -0,0 +1,365 @@
+//#include "MainWindow.h"
+
+
+#include <QtGui>
+
+class MainWindow : public QWidget
+{
+public:
+	MainWindow();
+	
+protected:
+
+};
+
+
+class TRGBFloat {
+public:
+	float R;
+	float G;
+	float B;
+};
+
+bool fillTriColor(QImage *img, QVector<QPointF> points, QList<QColor> colors)
+{
+	if(!img)
+		return false;
+		
+	if(img->format() != QImage::Format_ARGB32_Premultiplied ||
+	   img->format() != QImage::Format_ARGB32)
+		return false;
+		
+	double imgWidth  = img->width();
+	double imgHeight = img->height();
+	
+	/// The following routine translated from Delphi, originally found at:
+	// http://www.swissdelphicenter.ch/en/showcode.php?id=1780
+	
+	double  LX, RX, Ldx, Rdx, Dif1, Dif2;
+	
+	TRGBFloat LRGB, RRGB, RGB, RGBdx, LRGBdy, RRGBdy;
+		
+	QColor RGBT;
+	double y, x, ScanStart, ScanEnd;// : integer;
+	bool Right;
+	QPointF tmpPoint;
+	QColor tmpColor;
+	
+	
+	 // sort vertices by Y
+	int Vmax = 0;
+	if (points[1].y() > points[0].y())
+		Vmax = 1;
+	if (points[2].y() > points[Vmax].y())
+		 Vmax = 2;
+		 
+	if(Vmax != 2)
+	{
+		tmpPoint = points[2];
+		points[2] = points[Vmax];
+		points[Vmax] = tmpPoint;
+		
+		tmpColor = colors[2];
+		colors[2] = colors[Vmax];
+		colors[Vmax] = tmpColor;
+	}
+		
+	if(points[1].y() > points[0].y())
+		Vmax = 1;
+	else 
+		Vmax = 0;
+		
+	if(Vmax == 0)
+	{
+		tmpPoint = points[1];
+		points[1] = points[0];
+		points[0] = tmpPoint;
+		
+		tmpColor = colors[1];
+		colors[1] = colors[0];
+		colors[0] = tmpColor;
+	}
+	
+	Dif1 = points[2].y() - points[0].y();
+	if(Dif1 == 0)
+		Dif1 = 0.001; // prevent div by 0 error
+	
+	Dif2 = points[1].y() - points[0].y();
+	if(Dif2 == 0)
+		Dif2 = 0.001;
+
+	//work out if middle point is to the left or right of the line
+	// connecting upper and lower points
+	if(points[1].x() > (points[2].x() - points[0].x()) * Dif2 / Dif1 + points[0].x())
+		Right = true;
+	else	Right = false;
+	
+	   // calculate increments in x and colour for stepping through the lines
+	if(Right)
+	{
+		Ldx = (points[2].x() - points[0].x()) / Dif1;
+		Rdx = (points[1].x() - points[0].x()) / Dif2;
+		LRGBdy.B = (colors[2].blue()  - colors[0].blue())  / Dif1;
+		LRGBdy.G = (colors[2].green() - colors[0].green()) / Dif1;
+		LRGBdy.R = (colors[2].red()   - colors[0].red())   / Dif1;
+		RRGBdy.B = (colors[1].blue()  - colors[0].blue())  / Dif2;
+		RRGBdy.G = (colors[1].green() - colors[0].green()) / Dif2;
+		RRGBdy.R = (colors[1].red()   - colors[0].red())   / Dif2;
+	}
+	else
+	{
+		Ldx = (points[1].x() - points[0].x()) / Dif1;
+		Rdx = (points[2].x() - points[0].x()) / Dif2;
+		
+		RRGBdy.B = (colors[2].blue()  - colors[0].blue())  / Dif1;
+		RRGBdy.G = (colors[2].green() - colors[0].green()) / Dif1;
+		RRGBdy.R = (colors[2].red()   - colors[0].red())   / Dif1;
+		LRGBdy.B = (colors[1].blue()  - colors[0].blue())  / Dif2;
+		LRGBdy.G = (colors[1].green() - colors[0].green()) / Dif2;
+		LRGBdy.R = (colors[1].red()   - colors[0].red())   / Dif2;
+	}
+
+	LRGB.R = colors[0].red();
+	LRGB.G = colors[0].green();
+	LRGB.B = colors[0].blue();
+	RRGB = LRGB;
+	
+	LX = points[0].x();
+	RX = points[0].x();
+	
+	// fill region 1
+	for(y = points[0].y(); y <= points[1].y() - 1; y++)
+	{	
+		// y clipping
+		if(y > imgHeight - 1)
+			break;
+			
+		if(y < 0)
+		{
+			LX = LX + Ldx;
+			RX = RX + Rdx;
+			LRGB.B = LRGB.B + LRGBdy.B;
+			LRGB.G = LRGB.G + LRGBdy.G;
+			LRGB.R = LRGB.R + LRGBdy.R;
+			RRGB.B = RRGB.B + RRGBdy.B;
+			RRGB.G = RRGB.G + RRGBdy.G;
+			RRGB.R = RRGB.R + RRGBdy.R;
+			continue;
+		}
+		
+		// Scan = ABitmap.ScanLine[y];
+		
+		// calculate increments in color for stepping through pixels
+		Dif1 = RX - LX + 1;
+		if(Dif1 == 0)
+			Dif1 = 0.001;
+		RGBdx.B = (RRGB.B - LRGB.B) / Dif1;
+		RGBdx.G = (RRGB.G - LRGB.G) / Dif1;
+		RGBdx.R = (RRGB.R - LRGB.R) / Dif1;
+		
+		// x clipping
+		if(LX < 0)
+		{
+			ScanStart = 0;
+			RGB.B = LRGB.B + (RGBdx.B * fabs(LX));
+			RGB.G = LRGB.G + (RGBdx.G * fabs(LX));
+			RGB.R = LRGB.R + (RGBdx.R * fabs(LX));
+		}
+		else
+		{
+			RGB = LRGB;
+			ScanStart = LX; //round(LX);
+		} 
+		
+		if(RX - 1 > imgWidth - 1)
+			ScanEnd = imgWidth - 1;
+		else	ScanEnd = RX - 1; //round(RX) - 1;
+		
+
+		// scan the line
+		QRgb* scanline = (QRgb*)img->scanLine((int)y);
+		for(x = ScanStart; x <= ScanEnd; x++)
+		{
+			scanline[(int)x] = qRgb((int)RGB.R, (int)RGB.G, (int)RGB.B);
+			
+			RGB.B = RGB.B + RGBdx.B;
+			RGB.G = RGB.G + RGBdx.G;
+			RGB.R = RGB.R + RGBdx.R;
+		}
+		
+		// increment edge x positions
+		LX = LX + Ldx;
+		RX = RX + Rdx;
+		
+		// increment edge colours by the y colour increments
+		LRGB.B = LRGB.B + LRGBdy.B;
+		LRGB.G = LRGB.G + LRGBdy.G;
+		LRGB.R = LRGB.R + LRGBdy.R;
+		RRGB.B = RRGB.B + RRGBdy.B;
+		RRGB.G = RRGB.G + RRGBdy.G;
+		RRGB.R = RRGB.R + RRGBdy.R;
+	}
+
+	
+	Dif1 = points[2].y() - points[1].y();
+	if(Dif1 == 0)
+		Dif1 = 0.001;
+		
+	// calculate new increments for region 2
+	if(Right)
+	{
+		Rdx = (points[2].x() - points[1].x()) / Dif1;
+		RX  = points[1].x();
+		RRGBdy.B = (colors[2].blue()  - colors[1].blue())  / Dif1;
+		RRGBdy.G = (colors[2].green() - colors[1].green()) / Dif1;
+		RRGBdy.R = (colors[2].red()   - colors[1].red())   / Dif1;
+		RRGB.R = colors[1].red();
+		RRGB.G = colors[1].green();
+		RRGB.B = colors[1].blue();
+	}
+	else
+	{
+		Ldx = (points[2].x() - points[1].x()) / Dif1;
+		LX  = points[1].x();
+		LRGBdy.B = (colors[2].blue()  - colors[1].blue())  / Dif1;
+		LRGBdy.G = (colors[2].green() - colors[1].green()) / Dif1;
+		LRGBdy.R = (colors[2].red()   - colors[1].red())   / Dif1;
+		LRGB.R = colors[1].red();
+		LRGB.G = colors[1].green();
+		LRGB.B = colors[1].blue();
+	}
+
+	// fill region 2
+	for(y = points[1].y(); y < points[2].y() - 1; y++)
+	{
+		// y clipping
+		if(y > imgHeight - 1)
+			break;
+		if(y < 0)
+		{
+			LX = LX + Ldx;
+			RX = RX + Rdx;
+			LRGB.B = LRGB.B + LRGBdy.B;
+			LRGB.G = LRGB.G + LRGBdy.G;
+			LRGB.R = LRGB.R + LRGBdy.R;
+			RRGB.B = RRGB.B + RRGBdy.B;
+			RRGB.G = RRGB.G + RRGBdy.G;
+			RRGB.R = RRGB.R + RRGBdy.R;
+			continue;
+		}
+		
+		//Scan := ABitmap.ScanLine[y];
+		
+		Dif1 = RX - LX + 1;
+		if(Dif1 == 0)
+			Dif1 = 0.001;
+		
+		RGBdx.B = (RRGB.B - LRGB.B) / Dif1;
+		RGBdx.G = (RRGB.G - LRGB.G) / Dif1;
+		RGBdx.R = (RRGB.R - LRGB.R) / Dif1;
+		
+		// x clipping
+		if(LX < 0)
+		{
+			ScanStart = 0;
+			RGB.B = LRGB.B + (RGBdx.B * fabs(LX));
+			RGB.G = LRGB.G + (RGBdx.G * fabs(LX));
+			RGB.R = LRGB.R + (RGBdx.R * fabs(LX));
+		}
+		else
+		{
+			RGB = LRGB;
+			ScanStart = LX; //round(LX);
+		}
+		
+		if(RX - 1 > imgWidth - 1)
+			ScanEnd = imgWidth - 1;
+		else 	ScanEnd = RX - 1; //round(RX) - 1;
+		
+		// scan the line
+		QRgb* scanline = (QRgb*)img->scanLine((int)y);
+		for(x = ScanStart; x < ScanEnd; x++)
+		{
+			scanline[(int)x] = qRgb((int)RGB.R, (int)RGB.G, (int)RGB.B);
+			
+			RGB.B = RGB.B + RGBdx.B;
+			RGB.G = RGB.G + RGBdx.G;
+			RGB.R = RGB.R + RGBdx.R;
+		}
+		
+		LX = LX + Ldx;
+		RX = RX + Rdx;
+		
+		LRGB.B = LRGB.B + LRGBdy.B;
+		LRGB.G = LRGB.G + LRGBdy.G;
+		LRGB.R = LRGB.R + LRGBdy.R;
+		RRGB.B = RRGB.B + RRGBdy.B;
+		RRGB.G = RRGB.G + RRGBdy.G;
+		RRGB.R = RRGB.R + RRGBdy.R;
+	}
+	
+	return true;
+}
+
+MainWindow::MainWindow()
+	: QWidget()
+{
+	// Setup the layout of the window
+	QVBoxLayout *vbox = new QVBoxLayout(this);
+	vbox->setContentsMargins(0,0,0,0);
+	
+	QImage img(640,480,QImage::Format_ARGB32_Premultiplied);
+	 
+	QPainter p(&img);
+	
+	p.fillRect(img.rect(), Qt::black);
+	
+	QList<QColor> colors = QList<QColor>()
+		<< Qt::red
+		<< Qt::green
+		<< Qt::blue;
+		
+	double imgWidth  = img.width();
+	double imgHeight = img.height();
+
+
+	QVector<QPointF> points = QVector<QPointF>()
+		<< QPointF(10, 10)
+		<< QPointF(imgWidth - 10, imgHeight / 2)
+		<< QPointF(imgWidth / 2,  imgHeight - 10);
+
+// 	p.setBrush(colors[0]);
+// 	//p.setPen(colors[1]);
+// 	p.drawConvexPolygon(points);
+
+	fillTriColor(&img, points, colors);
+	
+	#define randRange(range) ( range * ((float) rand () / RAND_MAX) )
+	
+	QTime t;
+	t.start();
+	int max = 1000;
+	for(int i=0; i<max; i++)
+	{
+		QVector<QPointF> points = QVector<QPointF>()
+			<< QPointF(randRange(imgWidth), randRange(imgHeight))
+			<< QPointF(randRange(imgWidth), randRange(imgHeight))
+			<< QPointF(randRange(imgWidth), randRange(imgHeight));
+		
+		fillTriColor(&img, points, colors);
+	}
+	
+	int time = t.elapsed();
+	
+	double msPerTri = (double)time / (double)max;
+	qDebug() << "ms per tri: "<<msPerTri<<", total time elapsed for"<<max<<"triangles:"<<time; 
+	
+	img.save("sample.jpg");
+
+	QLabel *label = new QLabel();
+	label->setPixmap(QPixmap::fromImage(img));
+	vbox->addWidget(label);
+	
+	// Adjust window for a 4:3 aspect ratio
+	resize(640, 480);
+}
